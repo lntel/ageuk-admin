c2ba6ac509b9bbe567d65e705434c2e7bf312698
Joseph Harris <joeharris461767@gmail.com>
2022-07-17 22:22:57 +0100

    Add dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Template from '../../components/Template'
+
+const Dashboard = () => {
+  return (
+    <Template>
+      <p>test</p>
+    </Template>
+  )
+}
+
+export default Dashboard
