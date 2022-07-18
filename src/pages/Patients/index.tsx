81f5b284d7b6576fe7af3900849eeb2deed1a5b6
Joseph Harris <joeharris461767@gmail.com>
2022-07-18 23:29:42 +0100

    Add patient page Add patient page style

--- src/pages/Patients/index.tsx
+++ src/pages/Patients/index.tsx
@@ -1,331 +1,100 @@
-import { ColumnDef, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table'
-import React, { useState } from 'react'
-import './index.scss'
-import { Table } from '../../components/Table'
-import Template from '../../components/Template'
-import { Patient } from '../../types'
-import Paginator from '../../components/Paginator'
-import { MdMoreVert } from 'react-icons/md'
+import {
+  ColumnDef,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Table } from "../../components/Table";
+import Template from "../../components/Template";
+import { Patient } from "../../types";
+import Paginator from "../../components/Paginator";
+import { MdAddCircle, MdMoreVert } from "react-icons/md";
+
+const PatientActions = () => {
+
+  return (
+    <button className="patient-component__new">
+      <MdAddCircle />
+      Add New Patient
+    </button>
+  );
+}
 
 const Patients = () => {
+  const [selectedPatient, setSelectedPatient] = useState<Patient>();
+
+  useEffect(() => {
+    console.log(selectedPatient);
+  }, [selectedPatient]);
 
   const [patients, setPatients] = useState<Patient[]>([
     {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
+      surname: "Smith",
+      addressLine: "38 Windingbrook Lane",
+      county: "Northamptonshire",
+      postcode: "CH5 7GB",
+      firstName: "John",
+      middleNames: "",
+      startDate: new Date("2022-10-14 00:00:00"),
+      prognosis: "Weeks",
+      city: "Northampton",
+      id: "4501277104",
+      diagnoses: ["Stage 4 Leukemia"],
       generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
+      eightWeekReview: new Date("2022-12-02 00:00:00"),
+      sixWeekReview: new Date("2022-11-22 00:00:00"),
+      telephoneNumber: "01604700660",
+      dob: new Date("1965-06-14 00:00:00"),
+      gpFullname: "Unknown",
     },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-    {
-      surname: 'Smith',
-      addressLine: '38 Windingbrook Lane',
-      county: 'Northamptonshire',
-      postcode: 'CH5 7GB',
-      firstName: 'John',
-      middleNames: '',
-      startDate: new Date('2022-10-14 00:00:00'),
-      prognosis: 'Weeks',
-      city: 'Northampton',
-      id: '4501277104',
-      diagnoses: ['Stage 4 Leukemia'],
-      generalPractionerId: 6,
-      eightWeekReview: new Date('2022-12-02 00:00:00'),
-      sixWeekReview: new Date('2022-11-22 00:00:00'),
-      telephoneNumber: '01604700660',
-      dob: new Date('1965-06-14 00:00:00'),
-      gpFullname: 'Unknown',
-    },
-  ])
+  ]);
 
   const columns: ColumnDef<Patient>[] = [
     {
-      accessorKey: 'id',
-      cell: info => info.getValue(),
-      header: 'NHS Number'
+      accessorKey: "id",
+      cell: (info) => info.getValue(),
+      header: "NHS Number",
     },
     {
-      accessorKey: 'firstName',
-      cell: info => info.getValue(),
-      header: 'First Name'
+      header: "Patient Name",
+      accessorFn: row => `${row.firstName} ${row.surname}`
     },
     {
-      accessorKey: 'surname',
-      cell: info => info.getValue(),
-      header: 'Surname'
+      accessorKey: "diagnoses",
+      cell: (info) => info.getValue(),
+      header: "Diagnoses",
+    },
+    {
+      accessorKey: "prognosis",
+      cell: (info) => info.getValue(),
+      header: "Prognosis",
     },
     {
       accessorKey: 'addressLine',
-      cell: info => info.getValue(),
-      header: 'Address'
+      cell: (info) => info.getValue(),
+      header: "Address Line",
     },
     {
-      accessorKey: 'dob',
-      cell: info => new Date(info.getValue()).toLocaleDateString(),
-      header: 'DOB'
+      accessorKey: 'postcode',
+      cell: (info) => info.getValue(),
+      header: "Postcode",
     },
     {
-      id: 'Actions',
-      cell: props => [<MdMoreVert key={props.cell.id} />],
+      accessorKey: "dob",
+      cell: (info) => new Date(info.getValue()).toLocaleDateString(),
+      header: "DOB",
+    },
+    {
+      id: "Actions",
+      cell: (props) => [
+        <MdMoreVert
+          key={props.cell.id}
+          onClick={() => setSelectedPatient(props.getValue())}
+        />,
+      ],
     },
   ];
 
@@ -338,11 +107,16 @@ const Patients = () => {
   });
 
   return (
-    <Template header="Manage Patients" layout="none" className="patient-component">
-        <Table table={table} className="patient-component__table" />
-        <Paginator table={table} className="patient-component__paginator" />
+    <Template
+      header="Manage Patients"
+      layout="none"
+      className="patient-component"
+      headerChildren={<PatientActions />}
+    >
+      <Table table={table} className="patient-component__table" maxRows={6} />
+      <Paginator table={table} className="patient-component__paginator" />
     </Template>
-  )
-}
+  );
+};
 
-export default Patients
+export default Patients;
