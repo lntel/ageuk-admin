52f5764eb683ec8f947cb92b60a04792c33c047b
Joseph Harris <joeharris461767@gmail.com>
2022-07-18 22:06:05 +0100

    Add custom className Add layout prop

--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -3,17 +3,25 @@ import './index.scss'
 
 import Navigation from '../Navigation'
 import { Topbar } from '../Topbar';
+import classNames from 'classnames';
 
+export type TemplateLayout = | 'grid' | 'none';
 export interface TemplateProps {
     children?: any;
+    header?: string;
+    layout?: TemplateLayout;
+    className?: string;
 }
 
-const Template: FC<TemplateProps> = ({ children }) => {
+const Template: FC<TemplateProps> = ({ children, header, layout = 'grid', className }) => {
   return (
-    <main className="template">
+    <main className={classNames("template", className)}>
         <Navigation />
         <Topbar />
-        <div className="template__main">
+        <h1 className="template__header">
+          { header }
+        </h1>
+        <div className={classNames("template__main", layout == 'grid' ? "template__main--grid" : null)}>
             { children }
         </div>
     </main>
