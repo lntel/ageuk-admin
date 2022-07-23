f17b322e1e43e84e6f4c7f0025330c4b597c35be
Joseph Harris <joeharris461767@gmail.com>
2022-07-24 00:43:25 +0100

    Add input onChange handler

--- src/components/Textbox/index.tsx
+++ src/components/Textbox/index.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { FC } from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
 import './index.scss'
 
 type TextboxType = | 'text' | 'email' | 'password';
@@ -9,15 +9,26 @@ export interface TextboxProps {
     placeholder?: string;
     required?: boolean;
     className?: string;
+    value?: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Textbox: FC<TextboxProps> = ({ type = 'text', placeholder, className, required = false }) => {
+const Textbox: FC<TextboxProps> = ({ type = 'text', placeholder, className, value, required = false, onChange }) => {
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if(!onChange) return;
+
+    onChange(e);
+  }
+
   return (
     <>
     <input 
     type={type} 
     placeholder={placeholder}
     className={classNames("textbox", className)} 
+    value={value}
+    onChange={(e) => handleChange(e)}
     required={required} 
     />
     </>
