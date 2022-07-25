5908ad6791d5b2629aeb2e72b28b55afe3c78342
Joseph Harris <joeharris461767@gmail.com>
2022-07-25 21:48:31 +0100

    Add dropdown component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react'
+import classNames from 'classnames'
+import './index.scss'
+
+export interface IDropdownOption {
+    key: string;
+    value: string;
+}
+
+export interface DropdownProps {
+    className?: string;
+    placeholder?: string;
+    options: IDropdownOption[];
+    onSelect?: (value: string) => void;
+}
+
+const Dropdown: FC<DropdownProps> = ({ className, options, placeholder, onSelect }) => {
+
+    const handleSelect = (value: string) => {
+        if(!onSelect || value == "DROPDOWN_COMP_PLACEHOLDER") return;
+
+        onSelect(value);
+    }
+
+  return (
+    <select className={classNames("dropdown-component", className)} onChange={(e) => handleSelect(e.target.value)}>
+        { placeholder ? <option value="DROPDOWN_COMP_PLACEHOLDER">{ placeholder }</option> : null }
+        
+        { options.length ? options.map(({ key, value }) => 
+            <option value={value} key={value}>{ key }</option>
+        ) : null }
+    </select>
+  )
+}
+
+export default Dropdown
