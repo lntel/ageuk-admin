44dfbe86d74beb727992aa3a3dd521850aa921b1
Joseph Harris <joeharris461767@gmail.com>
2022-07-31 15:04:58 +0100

    Refactored

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,51 +1,92 @@
-import { flexRender, Table as TanTable } from '@tanstack/react-table'
-import classNames from 'classnames';
-import React, { FC, useEffect } from 'react'
-import './index.scss'
+import {
+  flexRender,
+  Table as TanTable,
+} from "@tanstack/react-table";
+import classNames from "classnames";
+import { FC, MouseEvent, useEffect, useState } from "react";
+import { MdMoreVert } from "react-icons/md";
+import ActionModal from "../ActionModal";
+import "./index.scss";
 
 export interface TableProps {
-    table: TanTable<any>;
-    className?: string;
-    maxRows?: number;
+  table: TanTable<any>;
+  className?: string;
+  maxRows?: number;
+  onActionClicked?: (x: number, y: number) => void;
 }
 
-export const Table: FC<TableProps> = ({ table, className, maxRows = 10 }) => {
+export const Table: FC<TableProps> = ({
+  table,
+  className,
+  onActionClicked,
+  maxRows = 10,
+}) => {
+  const [actionsVisible, setActionsVisible] = useState<boolean>(false);
 
-    useEffect(() => {
-        table.setPageSize(maxRows);
+  const handleActionClick = (e: MouseEvent<SVGAElement, MouseEvent>) => {
+    if (!onActionClicked) return;
 
-        console.log(`Max rows set to ${maxRows}`)
-    }, []);
+    onActionClicked(e.clientX, e.clientY);
+  };
+
+  useEffect(() => {
+    table.setPageSize(maxRows);
+
+    table.setOptions({
+      ...table.options,
+      columns: [
+        ...table.options.columns,
+        {
+          id: "Actions",
+          cell: (props) => [<MdMoreVert key={props.cell.id} onClick={() => console.log("test")} />],
+        },
+      ],
+    })
+    // table = useReactTable({
+    //   ...table.options,
+    //   columns: [
+    //     ...table.options.columns,
+    //     {
+    //       id: "Actions",
+    //       cell: (props) => [<MdMoreVert key={props.cell.id} />],
+    //     },
+    //   ],
+    // });
+
+    console.log(`Max rows set to ${maxRows}`);
+  }, []);
 
   return (
-    <table className={classNames("table", className)}>
+    <>
+      <table className={classNames("table", className)}>
         <thead className="table__header">
-        { table.getHeaderGroups().map(headerGroup => 
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-            { headerGroup.headers.map(header => 
+              {headerGroup.headers.map((header) => (
                 <th key={header.id}>
-                { header.isPlaceholder 
-                ? null 
-                : flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                ) }
-                </th>  
-            ) }
-            </tr>  
-        ) }
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
         <tbody>
-        { table.getRowModel().rows.map(row => 
+          {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
-            { row.getVisibleCells().map(cell => 
+              {row.getVisibleCells().map((cell) => (
                 <td key={cell.id}>
-                { flexRender(cell.column.columnDef.cell, cell.getContext()) }
-                </td>  
-            ) }
-            </tr>  
-        ) }
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
-    </table>
-  )
-}
+      </table>
+    </>
+  );
+};
