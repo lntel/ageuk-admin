d3f12dd7788ec044e6127224eceafac86fa126a8
Joseph Harris <joeharris461767@gmail.com>
2022-08-18 22:06:02 +0100

    Add value prop to dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -13,9 +13,10 @@ export interface DropdownProps {
     options: IDropdownOption[];
     "data-tip"?: string;
     onSelect?: (value: string) => void;
+    value: string;
 }
 
-const Dropdown: FC<DropdownProps> = ({ className, options, placeholder, onSelect, "data-tip": dataTip }) => {
+const Dropdown: FC<DropdownProps> = ({ className, options, placeholder, onSelect, "data-tip": dataTip, value }) => {
 
     const handleSelect = (value: string) => {
         if(!onSelect || value == "DROPDOWN_COMP_PLACEHOLDER") return;
@@ -24,7 +25,7 @@ const Dropdown: FC<DropdownProps> = ({ className, options, placeholder, onSelect
     }
 
   return (
-    <select className={classNames("dropdown-component", className)} onChange={(e) => handleSelect(e.target.value)} data-tip={dataTip}>
+    <select className={classNames("dropdown-component", className)} onChange={(e) => handleSelect(e.target.value)} data-tip={dataTip} value={value}>
         { placeholder ? <option value="DROPDOWN_COMP_PLACEHOLDER">{ placeholder }</option> : null }
         
         { options.length ? options.map(({ key, value }) => 
