d094b3102d19f6597cba35d861cea4edc39137af
Joseph Harris <joeharris461767@gmail.com>
2022-09-06 16:25:50 +0100

    Add generic context typing

--- src/context/MultiModalContext.tsx
+++ src/context/MultiModalContext.tsx
@@ -1,28 +1,29 @@
 import React, { createContext, FC, useEffect, useState } from 'react'
 
+// https://stackoverflow.com/a/60949057
 
 // TODO if possible, make the context strictly typed to an interface/generic this will make the dev experience much better
-export interface MultiModalContextType {
-    state: any;
-    setState: React.Dispatch<React.SetStateAction<any>>;
+export interface MultiModalContextType<T> {
+    state: T;
+    setState: React.Dispatch<React.SetStateAction<T>>;
 }
 
-export const MultiModalContext = createContext<MultiModalContextType>({
+export const MultiModalContext = createContext<MultiModalContextType<any>>({
     state: {},
     setState: (e) => {}
 });
 
-export interface MultiModalProviderProps {
+export interface MultiModalProviderProps<T> {
     children: any;
 }
 
-export const MultiModalProvider: FC<MultiModalProviderProps> = ({ children }) => {
+export const MultiModalProvider = <T extends object>(props: React.PropsWithChildren<MultiModalProviderProps<T>>) => {
 
-    const [state, setState] = useState<any>({});
-    
+    const [state, setState] = useState<T>();
+
     return (
         <MultiModalContext.Provider value={{ state, setState }}>
-            { children }
+            { props.children }
         </MultiModalContext.Provider>
     )
 }
